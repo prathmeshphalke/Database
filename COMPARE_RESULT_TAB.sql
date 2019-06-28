1432023ef95e94140d18634f908dab65e80c1e3b
prathmeshphalke <50272846+prathmeshphalke@users.noreply.github.com>
2019-06-27 20:52:17 -0400

    Update COMPARE_RESULT_TAB.sql

--- COMPARE_RESULT_TAB.sql
+++ COMPARE_RESULT_TAB.sql
@@ -1,4 +1,10 @@
-
+/*"""
+Author: Prathmesh Phalke
+Version: 2.4
+Created on: 04/22/2019
+Last Updated: 05/25/2019
+Description: Table that stores the result of table comparison done by DBcompare scripts. 
+"""*/
   CREATE TABLE "OBIEE_OWNER"."COMPARE_RESULTS" 
    (	"RUN_DATE" DATE, 
 	"TABLE_NAME" VARCHAR2(1000 BYTE), 
